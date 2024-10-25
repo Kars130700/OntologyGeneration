e1c647b21e280d2e30d428d7f40cfe8a9df02cb0
RavanStrijker <ravan.strijker@outlook.com>
2024-10-25 14:14:35 +0200

    Replaced api functions and compared speed

--- SNOMED.py
+++ SNOMED.py
@@ -2,12 +2,14 @@
 from urllib.request import urlopen, Request
 from urllib.parse import quote
 import json
+import time
 import matplotlib.pyplot as plt
 import networkx as nx
 from rdflib import Graph, Namespace, URIRef, Literal, RDF, RDFS, OWL
 import regex as re
 from visualizeOwls import visualize_owl
-from helperFunctions import getChildrenById, getSnomedConceptId, getParentsById, getConceptById
+from helperFunctions import getSnomedConceptId, getParentsById, getConceptById
+from queryDatabase import querySnomedConceptId, queryParentsById, queryConceptById
 # Note to self: maybe treat like a shortest route algorithm?
 
 baseUrl = 'https://browser.ihtsdotools.org/snowstorm/snomed-ct'
@@ -18,6 +20,7 @@ version = '2024-09-30'
 # Replace with a contact email so that we can contact you if your script causes excessive load on the public server
 # For example: user_agent = 'example@example.com'
 user_agent = 'tienvoortheorie@gmail.com'
+version = 'db'
 
 def BFS_ontology(graph, start, search_term):
     print(f"Starting BFS with {start[1]}")
@@ -27,7 +30,10 @@ def BFS_ontology(graph, start, search_term):
 
     while len(queue) > 0:
         current = queue.pop(0)
-        parents = getParentsById(current[0], search_term)
+        if version == 'db':
+            parents = queryParentsById(current[0], search_term)
+        else:
+            parents = getParentsById(current[0], search_term)
         print(parents)
 
         if len(parents) == 0:
@@ -60,12 +66,14 @@ def add_OWL_class(conc, g, ex, main_class):
     return g
 
 def sanitize_uri(name):
+    if (type(name) != str):
+        name = str(name)
     # Replace spaces with underscores and remove any invalid characters
     sanitized_name = re.sub(r'\s+', '_', name)  # Replace spaces with underscores
     sanitized_name = re.sub(r'[^a-zA-Z0-9_]', '', sanitized_name)  # Remove invalid characters
     return sanitized_name
 
-def buildOWL(input_dict, guideline_title, debug = True):
+def buildOWL(input_dict, guideline_title, debug = False):
     conc = sanitize_uri(guideline_title)
     ex = Namespace(f"http://example.org/{conc}#")
     g = Graph()
@@ -91,6 +99,8 @@ def buildOWL(input_dict, guideline_title, debug = True):
                       'eardrum': ['42859004', "Tympanic membrane structure (body structure)", ['eardrum', 'tympanic membrane structure', 'tympanic membrane']]}
 
     all_keys = list(input_dict.keys())
+    
+    start = time.time()
     for key, items in input_dict.items():
         other_keys = [k for k in all_keys if k != key]
         search_terms = other_keys
@@ -102,29 +112,62 @@ def buildOWL(input_dict, guideline_title, debug = True):
         else:
             parent_id = '123037004'
         g = add_OWL_class(items, g, ex, parent_id)
+    end = time.time()
+    print("BFS_ontolgy took", end - start, "seconds")
     g.serialize(destination='head_ontology.ttl', format='turtle')
 
 def most_common(lst):
     return max(set(lst), key=lst.count)
 
 def identify_root_IDs(input_dict: dict):
+    start = time.time()
     #We loop through the synonyms, gather all IDs and check the one that is most occuring
-    for key, value in input_dict.items():
-        ID_set = []
-        ID_set.append(getSnomedConceptId(key))
-        for synonym in value[2]:
-            ID_set.append(getSnomedConceptId(synonym))
-        value[0] = most_common(ID_set)
-        value[1] = getConceptById(value[0])
+    if version == 'db':
+        for key, value in input_dict.items():
+            ID_set = []
+            ID_set.append(querySnomedConceptId(key))
+            for synonym in value[2]:
+                ID_set.append(querySnomedConceptId(synonym))
+            value[0] = most_common(ID_set)
+            value[1] = queryConceptById(value[0])
+            print(f"Keyword '{key}' coupled to id {value[0]}")
+    else:
+        for key, value in input_dict.items():
+            ID_set = []
+            ID_set.append(getSnomedConceptId(key))
+            for synonym in value[2]:
+                ID_set.append(getSnomedConceptId(synonym))
+            value[0] = most_common(ID_set)
+            value[1] = getConceptById(value[0])
+            print(f"Keyword '{key}' coupled to id {value[0]}")
+    end = time.time()
+    print("identyfy_root_IDs took", end - start, "seconds")
     return input_dict
 
-dictionary = {
+
+dutchdictionary = {
+    'oor':['ID', "prefered label", ['oor', 'structuur van oor']],
+    'hoofd': ['ID', "prefered label",['hoofd', 'structuur van hoofd']], 
+    #'External Auditory Canal': ['ID', "prefered label",['External Auditory Canal']],
+    'trommelvlies': ['ID', "prefered label",['trommelvlies', 'membrana tympanica']]
+}
+
+englishdictionary = {
     'ear':['ID', "prefered label", ['ear', 'ear structure']],
     'head': ['ID', "prefered label",['head', 'head structure']], 
     #'External Auditory Canal': ['ID', "prefered label",['External Auditory Canal']],
-    #'Eardrum': ['ID', "prefered label",['eardrum']]
+    'eardrum': ['ID', "prefered label",['eardrum', 'tympanic membrane']]
 }
+
+if version == 'db':
+    dictionary = dutchdictionary
+else:
+    dictionary = englishdictionary
+
+start = time.time()
 buildOWL(dictionary, "Otitis Externa")
+end = time.time()
+print("Took", end - start, "seconds")
 
 #a: findings (symptomen)/disorders opzoeken in snomed - body parts die je nog niet hebt toevoegen\
 
@@ -145,3 +188,7 @@ input_dictionary = {
     'diagnoses': {'otitis externa': [None, None, ['uitwendige oorontsteking']], 'otitis media acuta': [None, None, ['acute middenoorontsteking']], 'furunkel': [None, None, ['boil']], 'herpes zoster oticus': [None, None, ['zoster oticus']], 'erysipelas': [None, None, ['rozevonk']], 'corpus alienum': [None, None, ['vreemd voorwerp']], 'cholesteatoom': [None, None, ['cholesteatoma']], 'gehoorgangcarcinoom': [None, None, ['oor carcinoom']]},
     'behandelingen': {'zure oordruppels': [None, None, ['acida oordruppels']], 'hydrocortison': [None, None, ['hydrocortisone']], 'triamcinolonacetonide': [None, None, ['triamcinolone acetonide']], 'flucloxacilline': [None, None, ['flucloxacillin']], 'occlusietherapie': [None, None, ['tamponneren']], 'uitspuiten van de gehoorgang': [None, None, ['cleaning of the ear canal']]}
 }
+
+# Results with debug=false and dictionary having ear, head and eardrum, each with 2 synonyms
+## DB version took about 38-40 seconds to finish, with identify_root_IDs taking about 38 seconds and BFS loop taking +- 0.01 seconds
+## API version took about 35-40 seconds to finish, with identify_root_IDs taking 16-18 seconds and BFS loop taking 17-19 seconds
--- queryDatabase.py
+++ queryDatabase.py
@@ -1,5 +1,6 @@
 import mysql.connector as mysql
 import os
+from helperFunctions import getSnomedConceptId, getParentsById, getConceptById
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -11,11 +12,50 @@ snomedDB = mysql.connect(
     password=os.getenv("MYSQL_PASSWORD"),
     database="snomedct"
 )
+cursor = snomedDB.cursor(buffered=True)
 
-cursor = snomedDB.cursor()
+# Helper function to get a SNOMED concept by name and return the ID, replaces helperFunctions.getSnomedConceptId
+def querySnomedConceptId(searchTerm):
+    query = f"SELECT conceptId, term FROM description as de WHERE de.active = 1 AND de.term = '{searchTerm}'"
+    cursor.execute(query)
+    
+    for (conceptId, term) in cursor:
+        if searchTerm.lower() == term.lower():
+            return conceptId
 
-cursor.execute("SELECT * FROM concept LIMIT 10")
-result = cursor.fetchall()
+    return None
 
-for x in result:
-    print(x)
+# Prints fsn of a concept, replaces helperFunction.getConceptById
+def queryConceptById(id):
+    query = f"SELECT term FROM description as de WHERE de.active = 1 AND de.conceptId = {id} AND de.typeId = 900000000000003001"
+    cursor.execute(query)
+    return cursor.fetchone()[0]
+
+def queryParentsById(conceptId, search_term, limit=8):
+    query = f"SELECT destinationId FROM relationship as re WHERE re.active = 1 AND re.sourceId = {conceptId} AND re.typeId = 116680003 LIMIT {limit}"
+    cursor.execute(query)
+    result = cursor.fetchall()
+
+    parents = []
+    for (destinationId) in result:
+        fsn = queryConceptById(destinationId[0])
+
+        for word in search_term:
+            if word in fsn:
+                return (True, word, destinationId[0])
+        parents.append((destinationId[0], fsn))
+    return parents
+    
+
+# Results: 
+## getSnomedConceptId('ear') takes about 2.3 seconds, querySnomedConceptId('oor') takes about 4.4 seconds
+### api call is 1.9 times faster
+
+## getConceptById(117590005) takes about 0.85 seconds, queryConceptById(117590005) takes 0 seconds
+### db query is infinitely faster
+
+## getParentsById("117590005", ["head"]) takes about 1.333 seconds, queryParentsById("117590005", ["hoofd"]) takes about 0.002 seconds
+### db query is 666.5 times faster
+
+## while loop with getParentsById takes about 6.150 seconds to find 'head' as a parent, same while loop with queryParentsById takes about 0.006 seconds to find 'hoofd' as a parent
+### db search is 1025 times faster
